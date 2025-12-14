443b193fe761fd1645ce3a536450907a9efda4b9
Kbgjtn <kambingjan31@gmail.com>
2025-12-14 10:22:25 +0000

    test: add test case in utf16_csu_test

--- utf16_csu_test.go
+++ utf16_csu_test.go
@@ -14,14 +14,15 @@ func TestAppendUTF16(t *testing.T) {
 		want    []uint16
 		wantErr error
 	}{
-		{name: "ascii characters", in: "A", want: []uint16{0x41}},
-		{name: "Latin character", in: "é", want: []uint16{0xE9}},
-		{name: "Euro sign", in: "€", want: []uint16{0x20AC}},
-		{name: "U+1F600", in: "😀", want: []uint16{0xD83D, 0xDE00}},
-		{name: "U+FFFD", in: string([]byte{0xEF, 0xBF, 0xBD}), want: []uint16{0xfffd}}, // replacement character
-		{name: "mixed string", in: "😀€éA", want: []uint16{0xD83D, 0xDE00, 0x20AC, 0xE9, 0x41}},
-		{name: "invalid single byte rune", in: string([]byte{0xFF}), want: make([]uint16, 0), wantErr: ErrInvalidUTF8},
-		{name: "truncated 3-byte sequence", in: string([]byte{0xE2, 0x82}), want: make([]uint16, 0), wantErr: ErrInvalidUTF8},
+		{"ascii characters", "A", []uint16{0x41}, nil},
+		{"Latin character", "é", []uint16{0xE9}, nil},
+		{"Euro sign", "€", []uint16{0x20AC}, nil},
+		{"U+1F600", "😀", []uint16{0xD83D, 0xDE00}, nil},
+		{"U+FFFD", string([]byte{0xEF, 0xBF, 0xBD}), []uint16{0xfffd}, nil}, // replacement character
+		{"mixed string", "😀€éA", []uint16{0xD83D, 0xDE00, 0x20AC, 0xE9, 0x41}, nil},
+		{"invalid single byte rune", string([]byte{0xFF}), make([]uint16, 0), ErrInvalidUTF8},
+		{"truncated 3-byte sequence", string([]byte{0xE2, 0x82}), make([]uint16, 0), ErrInvalidUTF8},
+		{"Truncated4Byte", string([]byte{0xF0, 0x9F, 0x92}), make([]uint16, 0), ErrInvalidUTF8},
 	}
 
 	for _, tc := range tests {
@@ -35,37 +36,10 @@ func TestAppendUTF16(t *testing.T) {
 	}
 }
 
-func randomString(n int, rng *rand.Rand) string {
-	runes := make([]rune, n)
-
-	for i := 0; i < n; i++ {
-		switch rng.Intn(3) {
-		// ASCII
-		case 0:
-			runes[i] = rune(rng.Intn(0x7F))
-
-		// BMP
-		case 1:
-			runes[i] = rune(0x80 + rng.Intn(0x7FF-0x80))
-
-		// Supplementary plane
-		case 2:
-			runes[i] = rune(0x10_000 + rng.Intn(0x10FFFF-0x10_000))
-		}
-	}
-
-	return string(runes)
-}
-
 func BenchmarkAppendUTF16(b *testing.B) {
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	var sizes []int = globalSizes
-	if testing.Short() {
-		sizes = []int{100, 1_000, 10_000, 15_000, 25_000, 50_000, 100_000}
-	}
-
-	for _, size := range sizes {
+	for _, size := range benchSizes() {
 		buf := make([]uint16, 0, size*2)
 		s := randomString(size, rng)
 
